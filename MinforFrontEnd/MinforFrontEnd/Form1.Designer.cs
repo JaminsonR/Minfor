0839b9d5d475e5ea2854d5edf947cb18f88a7241
JaminsonR <jamin.r.m@hotmail.com>
2016-10-26 23:50:21 -0500

    Ventana de login: visual terminado

--- MinforFrontEnd/MinforFrontEnd/Form1.Designer.cs
+++ MinforFrontEnd/MinforFrontEnd/Form1.Designer.cs
@@ -33,7 +33,8 @@
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.lblInicioSesion = new System.Windows.Forms.Label();
-            this.lblTitle = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtUsername
@@ -42,7 +43,7 @@
             this.txtUsername.BackColor = System.Drawing.SystemColors.Window;
             this.txtUsername.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtUsername.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtUsername.Location = new System.Drawing.Point(229, 176);
+            this.txtUsername.Location = new System.Drawing.Point(233, 213);
             this.txtUsername.MaxLength = 20;
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(167, 23);
@@ -55,7 +56,7 @@
             this.txtPassword.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtPassword.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPassword.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtPassword.Location = new System.Drawing.Point(229, 218);
+            this.txtPassword.Location = new System.Drawing.Point(233, 255);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(167, 23);
             this.txtPassword.TabIndex = 1;
@@ -69,9 +70,9 @@
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(247, 262);
+            this.btnLogin.Location = new System.Drawing.Point(242, 300);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(126, 28);
+            this.btnLogin.Size = new System.Drawing.Size(149, 31);
             this.btnLogin.TabIndex = 3;
             this.btnLogin.Text = "Iniciar Sesión";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -84,26 +85,24 @@
             this.lblInicioSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblInicioSesion.ForeColor = System.Drawing.Color.Black;
             this.lblInicioSesion.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.lblInicioSesion.Location = new System.Drawing.Point(224, 146);
+            this.lblInicioSesion.Location = new System.Drawing.Point(228, 183);
             this.lblInicioSesion.Name = "lblInicioSesion";
             this.lblInicioSesion.Size = new System.Drawing.Size(136, 25);
             this.lblInicioSesion.TabIndex = 4;
             this.lblInicioSesion.Text = "Inicio Sesión";
             this.lblInicioSesion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // lblTitle
+            // pictureBox1
             // 
-            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
-            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.ForeColor = System.Drawing.Color.Navy;
-            this.lblTitle.Location = new System.Drawing.Point(222, 72);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(185, 39);
-            this.lblTitle.TabIndex = 5;
-            this.lblTitle.Text = "Minfor S.A";
-            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(178, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Login
             // 
@@ -112,7 +111,7 @@
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(613, 472);
-            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblInicioSesion);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPassword);
@@ -122,6 +121,7 @@
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,7 +133,7 @@
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label lblInicioSesion;
-        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- MinforFrontEnd/MinforFrontEnd/Form1.cs
+++ MinforFrontEnd/MinforFrontEnd/Form1.cs
@@ -26,5 +26,10 @@ namespace MinforFrontEnd
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
