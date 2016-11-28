d9ce9e6ed71be0203b816839b9f3893318065675
JaminsonR <jamin.r.m@hotmail.com>
2016-11-28 00:32:43 -0500

    ventana de ingreso clientes

---
+++ MinforFrontEnd/MinforFrontEnd/frmIngresoCliente.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MinforFrontEnd
+{
+    partial class FrmIngresoCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIntrucciones = new System.Windows.Forms.Label();
+            this.lblNombres = new System.Windows.Forms.Label();
+            this.lblApellidos = new System.Windows.Forms.Label();
+            this.lblCedula = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblIntrucciones
+            // 
+            this.lblIntrucciones.AutoSize = true;
+            this.lblIntrucciones.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblIntrucciones.Location = new System.Drawing.Point(253, 56);
+            this.lblIntrucciones.Name = "lblIntrucciones";
+            this.lblIntrucciones.Size = new System.Drawing.Size(227, 17);
+            this.lblIntrucciones.TabIndex = 0;
+            this.lblIntrucciones.Text = "Ingrese los datos del nuevo cliente";
+            this.lblIntrucciones.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblNombres
+            // 
+            this.lblNombres.AutoSize = true;
+            this.lblNombres.Location = new System.Drawing.Point(152, 122);
+            this.lblNombres.Name = "lblNombres";
+            this.lblNombres.Size = new System.Drawing.Size(69, 17);
+            this.lblNombres.TabIndex = 1;
+            this.lblNombres.Text = "Nombres:";
+            // 
+            // lblApellidos
+            // 
+            this.lblApellidos.AutoSize = true;
+            this.lblApellidos.Location = new System.Drawing.Point(152, 165);
+            this.lblApellidos.Name = "lblApellidos";
+            this.lblApellidos.Size = new System.Drawing.Size(69, 17);
+            this.lblApellidos.TabIndex = 2;
+            this.lblApellidos.Text = "Apellidos:";
+            // 
+            // lblCedula
+            // 
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Location = new System.Drawing.Point(152, 215);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(56, 17);
+            this.lblCedula.TabIndex = 3;
+            this.lblCedula.Text = "Cedula:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(152, 267);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(51, 17);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "E-Mail:";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(227, 117);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(338, 22);
+            this.txtNombres.TabIndex = 5;
+            this.txtNombres.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(228, 160);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(338, 22);
+            this.txtApellidos.TabIndex = 6;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(228, 210);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(338, 22);
+            this.txtCedula.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(228, 262);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(338, 22);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // FrmIngresoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 444);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblCedula);
+            this.Controls.Add(this.lblApellidos);
+            this.Controls.Add(this.lblNombres);
+            this.Controls.Add(this.lblIntrucciones);
+            this.Name = "FrmIngresoCliente";
+            this.Text = "frmIngresoCliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIntrucciones;
+        private System.Windows.Forms.Label lblNombres;
+        private System.Windows.Forms.Label lblApellidos;
+        private System.Windows.Forms.Label lblCedula;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtEmail;
+    }
+}
